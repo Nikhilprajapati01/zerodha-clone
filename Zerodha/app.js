c2e82e6a61931fdc52cb738a158a5c15be41b646
nikhil_kumar <nkman2002@gmail.com>
2024-02-05 20:14:19 +0530

    js

--- Zerodha/app.js
+++ Zerodha/app.js
@@ -1,14 +1,28 @@
 const showmenu = document.querySelector('.ri-menu-line')
-const show = document.getElementsByClassName("menu")
 
-showmenu.addEventListener("click",function (){
-    if(show.style.display === "none"){
-         show.style.display = "block"
-         
-    }
-    else{
-        show.style.display = "none"
-    }
+// show.style.display = "none"
+
+function showclick(){
+
+    console.log("asdd")
+    document.getElementsByClassName("menu").classList.toggle("hide");
 }
 
-)
+showmenu.addEventListener("click",function(){
+      showclick()
+})
+
+
+
+
+// const check = true;
+// showmenu.addEventListener("click", function(){
+//     if(check == true){
+//         show.style.display = "block"
+//         check = false;
+//     }
+//     else{
+//         show.style.display = "none"
+//         check = true;
+//     }
+// })
